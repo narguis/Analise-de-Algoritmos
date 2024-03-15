59171ecc91f49149788663e8976829302f384d34
Guilherme Narguis <narguis.guilherme@gmail.com>
2024-03-15 18:36:57 -0300

    Busca Linear finalizada

--- busca_binaria.py
+++ busca_binaria.py
@@ -3,7 +3,7 @@ import random
 from math import floor
 
 
-# Functions defined
+# Definição das Funções
 
 def LinearSearch(array: list[int], size: int, key:int) -> int:
     for i in range(size):
@@ -38,94 +38,186 @@ def BinarySearch(sorted_array: list[int], size: int, key: int) -> int:
             return -1
 
 
-# Arrays de N aleatório (elementos não-únicos)
-n = {"n4": [random.randint(0,10*4) for _ in range(10*4)],
-     "n5": [random.randint(0,10*5) for _ in range(10*5)],
-     "n6": [random.randint(0,10*6) for _ in range(10*6)],
-     "n7": [random.randint(0,10*7) for _ in range(10*7)]
-     }
+# Arrays de N aleatório (elementos não-únicos) - Mais Rápido
+# n = {"n4": [random.randint(0,10**4) for _ in range(10**4)],
+#      "n5": [random.randint(0,10**5) for _ in range(10**5)],
+#      "n6": [random.randint(0,10**6) for _ in range(10**6)],
+#      "n7": [random.randint(0,10**7) for _ in range(10**7)],
+#      }
 
 # Sets de N (elementos únicos)
-# n4 = list(set(random.sample(range(10*5), 10*4)))
-# n5 = list(set(random.sample(range(10*6), 10*5)))
-# n6 = list(set(random.sample(range(10*7), 10*6)))
-# n7 = list(set(random.sample(range(10*8), 10*7)))
+n4 = list(set(random.sample(range(10**5), 10**4)))
+n5 = list(set(random.sample(range(10**6), 10**5)))
+n6 =  list(set(random.sample(range(10**7), 10**6)))
+n7 = list(set(random.sample(range(10**8), 10**7)))
 
-# Q chaves (não únicas)
-q = {"q4": [random.randint(0,10*4) for _ in range(10*4)],
-     "q5": [random.randint(0,10*5) for _ in range(10*5)],
-     "q6": [random.randint(0,10*6) for _ in range(10*6)],
-     "q7": [random.randint(0,10*7) for _ in range(10*7)]
-     }
+# Arrays de chaves Q (chaves não-únicas) - Mais Rápido
+# q = {"q2": [random.randint(0,10**7) for _ in range(10**2)],
+#      "q3": [random.randint(0,10**7) for _ in range(10**3)],
+#      "q4": [random.randint(0,10**7) for _ in range(10**4)],
+#      "q5": [random.randint(0,10**7) for _ in range(10**5)]
+#      }
+
+# Sets de chaves Q (chaves únicas)
+q2 = list(set(random.sample(range(10**7), 10**2)))
+q3 = list(set(random.sample(range(10**7), 10**3)))
+q4 = list(set(random.sample(range(10**7), 10**4)))
+q5 = list(set(random.sample(range(10**7), 10**5)))
 
 
 # BUSCA SEQUENCIAL
 # n = 10^4
-print("Busca sequencial:\nn = 10^4\n")
+total = 0
+print("BUSCA SEQUENCIAL:\n\nn = 10^4\n")
+
+    # q = 10 ^ 2
+start = timer()
+for i in range(len(q2)):
+    LinearSearch(n4, len(n4), q2[i])
+end = timer()
+print(f'q = 10^2, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
+
+    # q = 10 ^ 3
+start = timer()
+for i in range(len(q3)):
+    LinearSearch(n4, len(n4), q3[i])
+end = timer()
+print(f'q = 10^3, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
 
     # q = 10 ^ 4
 start = timer()
-for i in range(10**4):
-    LinearSearch(n['n4'], 10*4, random.randint(0, 10*4))
+for i in range(len(q4)):
+    LinearSearch(n4, len(n4), q4[i])
 end = timer()
-print(f'q = 10^4, Execução = {end - start:.2f}ms\n')
+print(f'q = 10^4, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
 
     # q = 10 ^ 5
 start = timer()
-for i in range(10**5):
-    LinearSearch(n['n4'], 10*5, random.randint(0, 10*4))
+for i in range(len(q5)):
+    LinearSearch(n4, len(n4), q5[i])
 end = timer()
-print(f'q = 10^5, Execução = {end - start:.2f}ms\n')
-    
-    # q = 10 ^ 6
-start = timer()
-for i in range(10**6):
-    LinearSearch(n['n4'], 10*6, random.randint(0, 10*4))
-end = timer()
-print(f'q = 10^6, Execução = {end - start:.2f}ms\n')
+print(f'q = 10^5, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
 
-    # q = 10 ^ 7
-start = timer()
-for i in range(10**7):
-    LinearSearch(n['n4'], 10*7, random.randint(0, 10*4))
-end = timer()
-print(f'q = 10^7, Execução = {end - start:.2f}ms\n')
+print(f'Total (n = 10^4) =  {total*1000:.2f}ms ou {total:.2f}s')
+print('.\n')
 
-# n = 10^4
-# Busca Sequencial
+# n = 10^5
+total = 0
+print("\nn = 10^5\n")
+
+    # q = 10 ^ 2
+start = timer()
+for i in range(len(q2)):
+    LinearSearch(n5, len(n5), q2[i])
+end = timer()
+print(f'q = 10^2, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
+
+    # q = 10 ^ 3
+start = timer()
+for i in range(len(q3)):
+    LinearSearch(n5, len(n5), q3[i])
+end = timer()
+print(f'q = 10^3, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
 
     # q = 10 ^ 4
-for i in range(10**4):
-    LinearSearch(n['n4'], 10*4, random.randint(0, 10*5))
+start = timer()
+for i in range(len(q4)):
+    LinearSearch(n5, len(n5), q4[i])
+end = timer()
+print(f'q = 10^4, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
 
     # q = 10 ^ 5
-for i in range(10**5):
-    LinearSearch(n['n5'], 10*5, random.randint(0, 10*6))
-    
-    # q = 10 ^ 6
-for i in range(10**6):
-    LinearSearch(n['n6'], 10*6, random.randint(0, 10*7))
+start = timer()
+for i in range(len(q5)):
+    LinearSearch(n5, len(n5), q5[i])
+end = timer()
+print(f'q = 10^5, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
 
-    # q = 10 ^ 7
-for i in range(10**7):
-    LinearSearch(n['n7'], 10*7, random.randint(0, 10*8))
+print(f'Total (n = 10^5) =  {total*1000:.2f}ms ou {total:.2f}s')
+print('.\n')
 
+# n = 10^6
+total = 0
+print("\nn = 10^6\n")
 
-# n = 10^4
-# Busca Sequencial
+    # q = 10 ^ 2
+start = timer()
+for i in range(len(q2)):
+    LinearSearch(n6, len(n6), q2[i])
+end = timer()
+print(f'q = 10^2, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
+
+    # q = 10 ^ 3
+start = timer()
+for i in range(len(q3)):
+    LinearSearch(n6, len(n6), q3[i])
+end = timer()
+print(f'q = 10^3, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
 
     # q = 10 ^ 4
-for i in range(10**4):
-    LinearSearch(n['n4'], 10*4, random.randint(0, 10*5))
+start = timer()
+for i in range(len(q4)):
+    LinearSearch(n6, len(n6), q4[i])
+end = timer()
+print(f'q = 10^4, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
 
     # q = 10 ^ 5
-for i in range(10**5):
-    LinearSearch(n['n5'], 10*5, random.randint(0, 10*6))
-    
-    # q = 10 ^ 6
-for i in range(10**6):
-    LinearSearch(n['n6'], 10*6, random.randint(0, 10*7))
+start = timer()
+for i in range(len(q5)):
+    LinearSearch(n6, len(n6), q5[i])
+end = timer()
+print(f'q = 10^5, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
 
-    # q = 10 ^ 7
-for i in range(10**7):
-    LinearSearch(n['n7'], 10*7, random.randint(0, 10*8))
+print(f'Total (n = 10^6) =  {total*1000:.2f}ms ou {total:.2f}s')
+print('.\n')
+
+# n = 10^7
+total = 0
+print("\nn = 10^7\n")
+
+    # q = 10 ^ 2
+start = timer()
+for i in range(len(q2)):
+    LinearSearch(n7, len(n7), q2[i])
+end = timer()
+print(f'q = 10^2, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
+
+    # q = 10 ^ 3
+start = timer()
+for i in range(len(q3)):
+    LinearSearch(n7, len(n7), q3[i])
+end = timer()
+print(f'q = 10^3, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
+
+    # q = 10 ^ 4
+start = timer()
+for i in range(len(q4)):
+    LinearSearch(n7, len(n7), q4[i])
+end = timer()
+print(f'q = 10^4, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
+
+    # q = 10 ^ 5
+start = timer()
+for i in range(len(q5)):
+    LinearSearch(n7, len(n7), q5[i])
+end = timer()
+print(f'q = 10^5, Execução = {(end - start)*1000:.2f}ms\n')
+total += end-start
+
+print(f'Total (n = 10^7) =  {total*1000:.2f}ms ou {total:.2f}s')
+print('.\n')
